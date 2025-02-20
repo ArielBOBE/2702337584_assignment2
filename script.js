c04e9a8947c31f6e8e9daac66fb27ba491196962
Ariel PD <arielprandi34315@gmail.com>
2025-02-20 22:51:03 +0700

    Starting over
    Signed-off-by: Ariel PD <arielprandi34315@gmail.com>

--- script.js
+++
@@ -1,6 +0,0 @@
-// On click call function to click a element
-document.getElementById("inactive_").onclick = function() {gotoSite()};
-
-function gotoSite() {
-    document.getElementById("inactive").click()
-}
