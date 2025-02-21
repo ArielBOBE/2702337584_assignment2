2ea56bbf564a93436603c71c725d257757c12086
Ariel PD <arielprandi34315@gmail.com>
2025-02-21 20:53:24 +0700

    finally done (not really kill me)
    Signed-off-by: Ariel PD <arielprandi34315@gmail.com>

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// To access the stars
+let stars = 
+    document.getElementsByClassName("star");
+let output = 
+    document.getElementById("output");
+
 document.getElementById("leftB").onclick = function() {minusImage()};
 document.getElementById("rightB").onclick = function() {plusImage()};
 
@@ -9,16 +15,16 @@ images[3] = "images/naruto.png"
 
 const desc = [[], [], [], []];
 desc[0][0] = "Ariel Prandi Darmawidjaja"
-desc[0][1] = "2702337584"
+desc[0][1] = "L4BC 2702337584"
 
 desc[1][0] = "Orlando Jonathan Padiman"
-desc[1][1] = "2702337615"
+desc[1][1] = "L4BC 2702337615"
 
 desc[2][0] = "ALMIRA FARAHIYAH SHAFIQA RANA"
-desc[2][1] = "2702378956"
+desc[2][1] = "L4CC 2702378956"
 
 desc[3][0] = "ABYAN ALI KARTASASMITA"
-desc[3][1] = "2702355385"
+desc[3][1] = "L4CC 2702355385"
 
 
 // index of images
@@ -52,4 +58,27 @@ function plusImage() {
     // student name and id
     document.getElementById("name").innerHTML = desc[index][0]
     document.getElementById("id").innerHTML = desc[index][1]
+}
+ 
+// Funtion to update rating
+function gfg(n) {
+    remove();
+    for (let i = 0; i < n; i++) {
+        if (n == 1) cls = "one";
+        else if (n == 2) cls = "two";
+        else if (n == 3) cls = "three";
+        else if (n == 4) cls = "four";
+        else if (n == 5) cls = "five";
+        stars[i].className = "star " + cls;
+    }
+    output.innerText = "Rating is: " + n + "/5";
+}
+ 
+// To remove the pre-applied styling
+function remove() {
+    let i = 0;
+    while (i < 5) {
+        stars[i].className = "star";
+        i++;
+    }
 }
